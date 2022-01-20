f168dc3df1da9f3c198393d545faa412c309ac63
Shahid Sarker <shahid.sarker@gmail.com>
2022-01-20 10:24:42 -0500

    form submission

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" v-model="email" required />
 
     <label>Password:</label>
     <input type="password" v-model="password" required />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -27,6 +28,10 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create an account</button>
+    </div>
   </form>
 
   <p>Email {{ email }}</p>
@@ -45,6 +50,7 @@ export default {
       terms: false,
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -58,6 +64,20 @@ export default {
     },
     removeSkill(e, skill) {
       this.skills = this.skills.filter((sk) => sk !== skill);
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 characters long";
+      if (!this.passwordError) {
+        console.log("email: ", this.email);
+        console.log("password: ", this.password);
+        console.log("role: ", this.role);
+        console.log("skills: ", this.skills);
+        console.log("terms accepted: ", this.terms);
+      }
     },
   },
 };
@@ -111,5 +131,26 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+button:hover {
+  cursor: pointer;
+  background: #4b90f7;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
